12832b9bf00b04c42870bdeb76bb4d29a10a82b3
Pablo Egido <pgarciaegido@gmail.com>
2021-12-28 18:53:35 +0100

    3 use shallow mount

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,17 +1,17 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import flushPromises from 'flush-promises';
 import App from '../src/App.vue';
 
 describe('App', () => {
     test('should render the title correctly', () => {
-        const wrapper = mount(App);
+        const wrapper = shallowMount(App);
         const title = wrapper.get('h1');
 
         expect(title.text()).toBe('Users list');
     });
 
     test('should render users fetched from service', async () => {
-        const wrapper = mount(App);
+        const wrapper = shallowMount(App);
         await flushPromises();
         const users = wrapper.findAllComponents('[data-test="user-item"]');
         expect(users.length).toBe(4);
