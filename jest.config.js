bb6aed981b4d543f673ad53a8365dedec9adf7a6
Pablo Egido <pgarciaegido@gmail.com>
2022-01-31 21:55:07 +0100

    coverage

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,13 @@ module.exports = {
     },
     moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    coverageDirectory: './tests/coverage',
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 90
+        }
+    },
 };
